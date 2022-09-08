e9545e4d47d31e4ee754d28349bf9c86923a494c
Steven Busenbarrick <sbusenba@gmail.com>
2022-09-08 12:13:40 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+mergeArrays = (arrayA,arrayB)=>{
+    let mergedArray = []
+    while ((arrayA.length>0) && (arrayB.length>0)){
+        console.log ("A and B are longer than 0")
+        console.log(`${arrayA[0]} vs ${arrayB[0]} `)
+        if (arrayA[0] >= arrayB[0]){
+            console.log (`${arrayA[0]} > ${arrayB[0]}`)
+            mergedArray.push(arrayB.shift())
+        } else {
+            console.log (`${arrayA[0]} < ${arrayB[0]}`)
+            mergedArray.push(arrayA.shift())
+        }
+        console.table(mergedArray)
+    } 
+    while ((arrayA.length>0)&&(arrayB.length == 0)){
+        mergedArray.push(arrayA.shift())
+    } 
+    while ((arrayA.length ==0)&& (arrayB.length >0)){
+        mergedArray.push(arrayB.shift())
+    }
+    console.log(mergedArray)
+return mergedArray;
+}
+
+arraySplit= (array)=>{
+        console.log(array.length)
+        let arrayA = array.slice(0,array.length/2)
+        console.log(arrayA)
+        let arrayB = array.slice(array.length/2)
+        console.log(arrayB)
+
+    return [arrayA,arrayB];
+}
+
+
+mergeSort=(array)=>{
+    //recursive case
+    
+    if (array.length >1){
+        let [arrayA,arrayB] = arraySplit(array);
+        let newArray = mergeArrays(mergeSort(arrayA),mergeSort(arrayB))
+        return newArray;
+    }
+    //base case
+    return array;
+}
