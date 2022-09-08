fe282d837862b3ed66d5dc37711f2623ccdb1903
Steven Busenbarrick <sbusenba@gmail.com>
2022-09-08 12:51:25 -0700

    remove console.log debugging

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ mergeArrays = (arrayA,arrayB)=>{
         } else {
             mergedArray.push(arrayA.shift())
         }
-        console.table(mergedArray)
     } 
     while ((arrayA.length>0)&&(arrayB.length == 0)){
         mergedArray.push(arrayA.shift())
@@ -14,24 +13,18 @@ mergeArrays = (arrayA,arrayB)=>{
     while ((arrayA.length ==0)&& (arrayB.length >0)){
         mergedArray.push(arrayB.shift())
     }
-    console.log(mergedArray)
 return mergedArray;
 }
 
 arraySplit= (array)=>{
-        console.log(array.length)
         let arrayA = array.slice(0,array.length/2)
-        console.log(arrayA)
         let arrayB = array.slice(array.length/2)
-        console.log(arrayB)
-
     return [arrayA,arrayB];
 }
 
 
 mergeSort=(array)=>{
     //recursive case
-    
     if (array.length >1){
         let [arrayA,arrayB] = arraySplit(array);
         let newArray = mergeArrays(mergeSort(arrayA),mergeSort(arrayB))
